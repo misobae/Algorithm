82313640472e762d9648d96c866f9fe00b8697ea
misobae <misobae0731@gmail.com>
2022-02-11 07:59:00 +0900

    Upload: 1110.js

---
+++ BOJ/step_4/1110.js
@@ -0,0 +1,13 @@
+let input = Number(require('fs').readFileSync('/dev/stdin').toString());
+let num = input;
+let sum;
+let answer = 0;
+while(true) {
+    answer++;
+
+    sum = Math.floor(num / 10) + (num % 10);
+    num = (num % 10) * 10 + (sum % 10);
+
+    if(input === num) break;
+}
+console.log(answer);
