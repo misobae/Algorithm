032099033850521f274a598338204604091ab794
misobae <misobae0731@gmail.com>
2022-02-17 07:55:45 +0900

    Upload: 2577

---
+++ BOJ/step_5/2577.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const num = String(input[0] * input[1] * input[2]);
+
+for(let i = 0; i <= 9; i++) {
+  let count = 0;
+  
+  for(let j = 0; j < num.length; j++) {
+      if(Number(num[j]) === i) {
+          count++;
+      }
+  }
+  console.log(count);
+}
